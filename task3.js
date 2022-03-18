c5091b408fb1fb5df3de90d38b4769db8b9a8a8a
SSSrepos <skyreacherdude55@gmail.com>
2022-03-18 23:37:40 +1030

    Added mods for task 3-3

--- task3.js
+++ task3.js
@@ -8,9 +8,9 @@ mCountDiv.onmouseenter = function(){    //upon mouse entering the div, deploy fu
 const date = new Date();
 
 
-var button = document.getElementById("postButton")
-button.onclick = function changeTextStyle(){  //WTF
-    //
+var postButton = document.getElementById("postButton")
+postButton.onclick = function changeTextStyle(){ 
+    //create variables for values to be used later
     let postText = document.getElementsByTagName('textarea')[0].value;
     let localDate = date.toLocaleString();
     //create child <p> element
@@ -22,9 +22,22 @@ button.onclick = function changeTextStyle(){  //WTF
     var parent = document.getElementById('posts');
     parent.appendChild(postTime);
     parent.appendChild(postContent);
-    //change text in element
+    //change text in element to be correct content and styling
     postContent.innerText = postText;
     postTime.innerText = localDate;
     postTime.style.color = "grey";
     postTime.style.fontWeight = "bold";
+}
+
+var menuButton = document.getElementsByClassName("right")[0]; //menu button - 0; back button - 1;
+var backButton = document.getElementsByClassName("right")[1];
+var mainDiv = document.getElementById("main");
+var menuDiv = document.getElementById("menu");
+menuButton.onclick = function hideMainDiv(){
+    mainDiv.style.display = "none";
+    menuDiv.style.display = "inline";
+}
+backButton.onclick = function revealMainDiv(){
+    mainDiv.style.display = "inline";
+    menuDiv.style.display = "none";
 }
