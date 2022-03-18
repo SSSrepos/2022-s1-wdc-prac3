12f4d80356df972664abe6b0f13846f27de87f32
SSSrepos <skyreacherdude55@gmail.com>
2022-03-18 20:20:53 +1030

    Adding Task 3.2 update to the main branch

--- task3.js
+++ task3.js
@@ -1,6 +1,28 @@
-var mouseInCount = 0;
-const mCountDiv = document.getElementById("mcount"); 
-mCountDiv.onmouseenter = function(){
-    mouseInCount++;
-    mCountDiv.innerHTML = mouseInCount; 
+var mouseInCount = 0;       //set variable for counting the # of times mouse has entered blue div
+const mCountDiv = document.getElementById("mcount");    //select the div
+mCountDiv.onmouseenter = function(){    //upon mouse entering the div, deploy functiom
+    mouseInCount++;                     //increment counter
+    mCountDiv.innerHTML = mouseInCount; //display counter in blue div
+}
+
+const date = new Date();
+
+
+var button = document.getElementById("postButton")
+button.onclick = function changeTextStyle(){  //WTF
+    //
+    let postText = document.getElementsByTagName('textarea')[0].value;
+    let localDate = date.toLocaleString();
+    //create child <p> element
+    let postTime = document.createElement('p');
+    let postContent = document.createElement('p');
+    //attach child to parent div element
+    var parent = document.getElementById('posts');
+    parent.appendChild(postTime);
+    parent.appendChild(postContent);
+    //change text in element
+    postContent.innerText = postText;
+    postTime.innerText = localDate;
+    postTime.style.color = "grey";
+    postTime.style.fontWeight = "bold";
 }
