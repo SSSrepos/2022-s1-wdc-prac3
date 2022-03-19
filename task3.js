f3535e046e6691e52f16aa34f0fa8701527c23cb
SSSrepos <skyreacherdude55@gmail.com>
2022-03-19 14:42:13 +1030

    Added mods for task 3.4

--- task3.js
+++ task3.js
@@ -5,30 +5,53 @@ mCountDiv.onmouseenter = function(){    //upon mouse entering the div, deploy fu
     mCountDiv.innerHTML = mouseInCount; //display counter in blue div
 }
 
+
+//post submit function -- has color, number of posts considered
 const date = new Date();
 
+var postButton = document.getElementById("postButton");
 
-var postButton = document.getElementById("postButton")
-postButton.onclick = function changeTextStyle(){ 
-    //create variables for values to be used later
-    let postText = document.getElementsByTagName('textarea')[0].value;
-    let localDate = date.toLocaleString();
-    //create child <p> element
-    let postTime = document.createElement('p');
-    postTime.className = "post-time";
-    let postContent = document.createElement('p');
-    postContent.className = "post-content"
-    //attach child to parent div element
-    var parent = document.getElementById('posts');
-    parent.appendChild(postTime);
-    parent.appendChild(postContent);
-    //change text in element to be correct content and styling
-    postContent.innerText = postText;
-    postTime.innerText = localDate;
-    postTime.style.color = "grey";
-    postTime.style.fontWeight = "bold";
+postButton.onclick = function createPost(){
+    
+    //attach child (or children if numeric input says more than 1) to parent div element
+    var numberOfPosts = document.getElementById("gamer").value;
+    for(let i = 0; i < numberOfPosts; i++){
+        
+        var parent = document.getElementById('posts');
+        var numberOfPosts = document.getElementById("numOfPosts").value;
+        
+        //create variables for values to be used later
+        let postText = document.getElementsByTagName('textarea')[0].value;
+        let localDate = date.toLocaleString();
+        
+        //create child <p> element
+        let postTime = document.createElement('p');
+        postTime.className = "post-time";
+        let postContent = document.createElement('p');
+        postContent.className = "post-content";
+        
+        //set post time fontweight and color
+        postTime.style.color = "grey";
+        postTime.style.fontWeight = "bold";
+        
+        //check if color radio buttons are clicked
+        let radioColorOptions = document.getElementsByName("color");
+        if(radioColorOptions[0].checked){
+            postContent.style.color = "blue";
+        }else if(radioColorOptions[1].checked){
+            postContent.style.color = "red";
+        }
+        
+        //append and paragraph
+        parent.appendChild(postTime);
+        parent.appendChild(postContent);
+        postContent.innerText = postText;
+        postTime.innerText = localDate;
+    }
 }
 
+/* main div hide and unhide function
+*/
 var menuButton = document.getElementsByClassName("right")[0]; //menu button - 0; back button - 1;
 var backButton = document.getElementsByClassName("right")[1];
 var mainDiv = document.getElementById("main");
